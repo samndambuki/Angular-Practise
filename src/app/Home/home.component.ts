d53f143bc0928bad8da3e830d7d7630ea87af706
samndambuki <sam.ndambuki08@gmail.com>
2023-05-20 15:25:27 +0300

    directives complete

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,13 +1,14 @@
-import { Component, ViewChild, ViewEncapsulation } from "@angular/core";
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from "@angular/core";
 
 @Component(
     {
-        selector:'.app-home',
+        selector:'app-home',
         // template:`<h1>
         //     Hello Sam
         // </h1>
         // `,
         templateUrl:'./home.component.html',
+        // styleUrls:'./home.component.css',
         styles:[
             `h1{
                 color:red
@@ -18,27 +19,53 @@ import { Component, ViewChild, ViewEncapsulation } from "@angular/core";
 )
 
 export class HomeComponent{
-    message:string="Hello Message"   
-    showBtn = false //condition
-    inputText = 'Two way data binding'
+    // message:string="Hello Message"   
+    // showBtn = false //condition
+    // inputText = 'Two way data binding'
+    // @ViewChild('input') input!:ElementRef
 
-    // @ViewChild() //view things form ts
+    // // @ViewChild() //view things form ts
 
-    constructor(){
-        setTimeout(()=>{
-            this.showBtn=true
-        },3000)
+    // constructor(){
+    //     setTimeout(()=>{
+    //         this.showBtn=true
+    //     },3000)
+    // }
+    // getMessage(){
+    //     return 0
+    // }
+    // changeMessage(){
+    //     // this.message = 'message'
+    //     // this.input
+    //     // console.log(this.input);
+    //     this.input.nativeElement.value
+    // }
+
+    // getInput(event:Event){
+    //     let value = (event.target as HTMLInputElement).value
+    //     console.log(value);
+    //     this.message = value
+    // }
+
+    users:{name:string,age:number}[] = [
+        {
+            name:"Samuel Ndambuki",
+            age:21
+        }
+    ]
+
+    day = new Date().getDay()
+
+    addStudent(name:string,age:string){
+        this.users.push({name,age:+age})
+        // console.log(this.day);
+        this.getColor()
     }
-    getMessage(){
-        return 0
-    }
-    changeMessage(){
-        this.message = 'message'
+    deleteUser(i:number){
+        this.users.splice(i,1)
     }
 
-    getInput(event:Event){
-        let value = (event.target as HTMLInputElement).value
-        console.log(value);
-        this.message = value
+    getColor(){
+         return Math.floor(Math.random()*1100)>500? "green":"red"
     }
 }
